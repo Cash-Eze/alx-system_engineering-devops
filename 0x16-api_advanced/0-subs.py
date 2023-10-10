47ae138703fa039b673b9e2ec228c0139cf6c14f
Amos Hiamatsu <amoshiamatsu@gmail.com>
2023-10-10 12:51:19 -0700

    reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Module for task 0"""
 
+"""working on a module"""
 
 def number_of_subscribers(subreddit):
-        """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-            import requests
+    """Queries the Reddit API and returns the number of subscribers
+        to the subreddit"""
+    import requests
 
-                sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                                                                    .format(subreddit),
-                                                                    headers={"User-Agent": "My-User-Agent"},
-                                                                    allow_redirects=False)
-                    if sub_info.status_code >= 300:
-                                return 0
+    sub_info = requests.get(path.format(subreddit),
+                            headers = {"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
 
-                                return sub_info.json().get("data").get("subscribers")
+    if sub_info.status_code >= 300:
+        return 0
+
+    return (sub_info.json().get("data").get("subscriber"))
