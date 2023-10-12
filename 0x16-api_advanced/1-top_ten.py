3c91df27668d8619c86196cd250e2e398afa7659
Cash-Eze <amoshiamatsu@gmail.com>
2023-10-11 17:36:07 -0700

    top ten queries

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""Module for Api"""
+import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-       of the subreddit"""
-       import requests
+    """requests for top ten qeuries"""
+    path = "https://www.reddit.com/r/{}/hot?limit=10.json"
 
-       sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                               .format(subreddit),
-                               headers={"User-Agent": "My-User-Agent"},
-                               allow_redirects=False)
-       if sub_info.status_code >= 300:
-           print('None')
-       else:
-           [print(child.get("data").get("title"))
-           for child in sub_info.json().get("data").get("children")]
+    sub_info = requests.get(path.format(subreddit),
+            hearders= { "User-Agent": "My-User-Agent"},
+            allow_redirects=False)
+
+    if sub_info.status_code >= 300:
+        print(None)
+    else:
+        [print(child.get("data").get("title"))
+                for child in sub_info.json().get("data").get("children")]
