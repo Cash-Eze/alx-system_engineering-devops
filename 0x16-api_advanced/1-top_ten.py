005e6653d3d35c4ef870fc3c3dc2953a4e82b5e8
Cash-Eze <amoshiamatsu@gmail.com>
2023-10-11 18:06:58 -0700

    corrections

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,11 @@ def top_ten(subreddit):
     path = "https://www.reddit.com/r/{}/hot?limit=10.json"
 
     sub_info = requests.get(path.format(subreddit),
-            hearders= { "User-Agent": "My-User-Agent"},
-            allow_redirects=False)
+                            headers= { "User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
 
     if sub_info.status_code >= 300:
         print(None)
     else:
         [print(child.get("data").get("title"))
-                for child in sub_info.json().get("data").get("children")]
+        for child in sub_info.json().get("data").get("children")]
