d5fac6b86faf306ed2b67124da22233e9b7f0d83
Cash-Eze <amoshiamatsu@gmail.com>
2023-10-18 19:32:56 -0700

    web debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,12 @@
-# Fix the nginx requests limit
-exec { 'upgrade':
-  path    => '/bin/',
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+# Set ulimit on nginx max file descriptors to system soft limit
+exec {'ulimit':
+  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /etc/default/nginx',
 }
 
-exec { 'restart':
-  path    => '/usr/bin/',
+exec {'restart nginx':
   command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  require => Exec['ulimit'],
 }
